cd0fba8006d900302c2aa18d8ad263c917250fea
paul-blackwell <paul@paulblackwell.io>
2024-09-17 08:32:42 +0100

    Chapter 2 - Caching, Data Fetching and Mutations
    Actions:
    
    + 4. Server Actions

--- sprout-scribble/app/error.tsx
+++ sprout-scribble/app/error.tsx
@@ -14,7 +14,7 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
+    <div className="w-full min-h-56 flex items-center justify-center flex-col">
       <h2>Something went wrong! {error.message}</h2>
       <button onClick={() => reset()}>Try again</button>
     </div>
