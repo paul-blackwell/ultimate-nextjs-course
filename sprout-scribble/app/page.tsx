1a064558ec7ad542790c24635bd40932f0bf4e90
paul-blackwell <paul@paulblackwell.io>
2024-09-20 08:06:07 +0100

    Chapter 2 - Caching, Data Fetching and Mutations
    Actions:
    
    + 6. Progressively Enhanced Forms

--- sprout-scribble/app/page.tsx
+++ sprout-scribble/app/page.tsx
@@ -1,29 +1,7 @@
-import getPosts from '@/server/actions/get-posts';
-import createPost from '@/server/actions/create-post';
-
 export default async function Home() {
-  const { error, data } = await getPosts();
-
-  if (error) {
-    throw new Error(error);
-  }
-
-  if (data) {
-    return (
-      <main>
-        {data.map((post) => (
-          <div key={post.id}>{post.title}</div>
-        ))}
-        <form action={createPost}>
-          <input
-            className="bg-black"
-            type="text"
-            name="title"
-            placeholder="Title"
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <h1>Home Page</h1>
+    </main>
+  );
 }
