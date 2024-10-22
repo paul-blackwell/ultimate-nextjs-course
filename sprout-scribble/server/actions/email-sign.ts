e8b9a31dc52831ce8c5253087d7d6fc037499274
paul-blackwell <paul@paulblackwell.io>
2024-10-22 08:36:45 +0100

    Chapter 4 - Authentication with Next-Auth V5
    Actions:
    
    + 12.Form Error and Success Components

--- sprout-scribble/server/actions/email-sign.ts
+++ sprout-scribble/server/actions/email-sign.ts
@@ -2,10 +2,21 @@
 
 import { actionClient } from '@/lib/safe-action';
 import { LoginSchema } from '@/types/login-schema';
+import { db } from '..';
+import { eq } from 'drizzle-orm';
+import { users } from '@/server/schema';
 
 export const emailSignIn = actionClient
   .schema(LoginSchema)
   .action(async ({ parsedInput: { email, password, code } }) => {
-    console.log(email, password, code);
-    return { email };
+    // Check if user is in db
+    const existingUser = await db.query.users.findFirst({
+      where: eq(users.email, email),
+    });
+
+    if (existingUser?.email === email) {
+      return { error: 'User not found' };
+    }
+
+    return { success: email };
   });
