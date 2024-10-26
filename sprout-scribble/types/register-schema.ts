42361b13ecd911d2957c944e068c77e0b5ec0c6d
paul-blackwell <paul@paulblackwell.io>
2024-10-26 08:26:38 +0100

    Chapter 4 - Authentication with Next-Auth V5
    Actions:
    
    +  13.Creating Our Register Page (100%)

--- sprout-scribble/types/register-schema.ts
+++ sprout-scribble/types/register-schema.ts
@@ -1,6 +1,6 @@
 import * as z from 'zod';
 
-export const RegisterSchema = {
+export const RegisterSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8, {
     message: 'Password must be at least 8 characters long',
@@ -8,4 +8,4 @@ export const RegisterSchema = {
   name: z
     .string()
     .min(3, { message: 'Please add a name with at least 4 characters' }),
-};
+});
