442d857012fa7ed09f1e1f32c065fb2ad8ed6e53
paul-blackwell <paul@paulblackwell.io>
2024-10-28 08:02:47 +0000

    Chapter 4 - Authentication with Next-Auth V5
    Actions:
    
    + 15.Email Token Drizzle Schema (100%)

--- sprout-scribble/server/schema.ts
+++ sprout-scribble/server/schema.ts
@@ -53,3 +53,17 @@ export const accounts = pgTable(
     }),
   })
 );
+
+export const emailTokens = pgTable(
+  'email_tokens',
+  {
+    id: text('id').notNull(),
+    token: text('token').notNull(),
+    expires: timestamp('expires', { mode: 'date' }).notNull(),
+  },
+  (verificationToken) => ({
+    compositePk: primaryKey({
+      columns: [verificationToken.id, verificationToken.token],
+    }),
+  })
+);
