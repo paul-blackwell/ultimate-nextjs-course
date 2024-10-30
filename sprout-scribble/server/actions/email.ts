4a111ea4d644e15782cebf184bb12a5ab6a29ad4
paul-blackwell <paul@paulblackwell.io>
2024-10-30 08:33:40 +0000

    Chapter 4 - Authentication with Next-Auth V5
    Actions:
    
    + 17.Resend (60%)

--- sprout-scribble/server/actions/email.ts
+++ sprout-scribble/server/actions/email.ts
@@ -5,4 +5,15 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = getBaseUrl();
 
-export const sendVerificationEmail = async (email: string, token: string) => {};
+export const sendVerificationEmail = async (email: string, token: string) => {
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
+
+  const { data, error } = await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: email,
+    subject: 'Sprout and Scribble - Confirmation Email',
+    html: `<p>Click to <a href="${confirmLink}">confirm your email address</a>.</p>`,
+  });
+  if (error) return console.error(error);
+  if (data) return data;
+};
